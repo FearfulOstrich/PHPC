2690ce5fec0fcde2485f59e52bdafd7c4ac9d6bc
Guillaume Alleon <guillaume.alleon@airbus.com>
2017-05-09 19:25:39 +0200

    Attempt by Titou to change the matrix dimensions

--- GS_seq.cpp
+++ GS_seq.cpp
@@ -1,51 +1,58 @@
 #include<iostream>
- 
+#include<iomanip>
+#include<cmath>
+#include<cstdlib>
+
 using namespace std;
- 
-int main(void)
+int main()
 {
-    float a[10][10], b[10], x[10], y[10];
-    int n = 0, m = 0, i = 0, j = 0;
-    cout << "Enter size of 2d array(Square matrix) : ";
-    cin >> n;
+    cout.precision(4);
+    cout.setf(ios::fixed);
+    int n,i,j,k,temp=0,flag=0,count=0;
+    cout<<"\nEnter the no. of equations : ";           
+    cin>>n;                    //Input no. of equations
+    double a[n][n+1];            //declare a 2d array for storing the elements of the augmented matrix
+    double x[n];                //declare an array to store the values of variables
+    double eps,y;
+    for (i=0;i<n;i++)
+        for (j=0;j<n;j++)
+            a[i][j]=10*((rand()/RAND_MAX)-0.5);
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        temp=0;
+        for (j=0;j<n; j++)
         {
-            cout << "Enter values no :(" << i << ", " << j << ") ";
-            cin >> a[i][j];
+            if (i!=j)
+                temp = temp+abs(a[i][j]);
         }
+        a[i][i]=temp;
     }
-    cout << "\nEnter Values to the right side of equation\n";
-    for (i = 0; i < n; i++)
+    for (i=0;i<n;i++)
+        x[i]=100*((rand()/RAND_MAX)-0.5);
+    cout<<"\nEnter the accuracy upto which you want the solution: ";
+    cin>>eps;
+    cout<<"\n";
+    do                            //Perform iterations to calculate x1,x2,...xn
     {
-        cout << "Enter values no :(" << i << ", " << j << ") ";
-        cin >> b[i];
-    }
-    cout << "Enter initial values of x\n";
-    for (i = 0; i < n; i++)
-    {
-        cout << "Enter values no. :(" << i<<"):";
-        cin >> x[i];
-    }
-    cout << "\nEnter the no. of iteration : ";
-    cin >> m;
-    while (m > 0)
-    {
-        for (i = 0; i < n; i++)
+        for (i=0;i<n;i++)                //Loop that calculates x1,x2,...xn
         {
-            y[i] = (b[i] / a[i][i]);
-            for (j = 0; j < n; j++)
+            y=x[i];
+            x[i]=a[i][n];
+            for (j=0;j<n;j++)
             {
-                if (j == i)
-                    continue;
-                y[i] = y[i] - ((a[i][j] / a[i][i]) * x[j]);
-                x[i] = y[i];
+                if (j!=i)
+                x[i]=x[i]-a[i][j]*x[j];
             }
-            printf("x%d = %f    ", i + 1, y[i]);
+            x[i]=x[i]/a[i][i];
+            if (abs(x[i]-y)<=eps)            //Compare the ne value with the last value
+                flag++;
         }
-        cout << "\n";
-        m--;
-    }
+        count++;   
+    }while(flag<n);                        //If the values of all the variables don't differ from their previious values with error more than eps then flag must be n and hence stop the loop
+   
+    cout<<"\n The solution is as follows:\n";
+    for (i=0;i<n;i++)
+        cout<<"x"<<i<<" = "<<x[i]<<endl;    //Print the contents of x[]
+    cout << "\n ended in : " << count << " iterations.";       
     return 0;
 }
